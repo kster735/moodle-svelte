fcb3aa386d792a16162b4550b268ef3d8e1af329
kster735 <77456812+kster735@users.noreply.github.com>
2024-08-23 18:25:58 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+   function showMsg() {
+     alert("You clicked the button?");
+   }
+</script>
+
+<h1> This is the html part </h1>
+<button on:click={showMsg} > 
+  Message
+</button>
+
+<style>
+  h1 {
+    margin: 32px;
+  }
+
+  button {
+    background-color: cyan;
+    color: red;
+    margin: 32px;
+    padding: 8px 16px;
+  }
+</style>
